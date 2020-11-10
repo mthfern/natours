8aa054bee80c5a025ff3f62c79ec04dfb0225288
mthfern <66393031+mthfern@users.noreply.github.com>
2020-11-09 22:38:14 -0300

    added API/createTour function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,17 +1,4 @@
-// const fs = require('fs');
-// const Tour = require('../models/tourModel');
-
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-exports.checkBody = (req, res, next) => {
-  const { body } = req;
-  if (!(body.name && body.price)) {
-    return res.status(400).json({ status: 'fail', message: 'invalid data' });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -25,10 +12,19 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createNewTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
