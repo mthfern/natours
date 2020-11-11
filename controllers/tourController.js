8bb893cd71b6109886575486af91d34be5fa86e3
mthfern <66393031+mthfern@users.noreply.github.com>
2020-11-10 22:14:24 -0300

    Upgraded tour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,36 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -27,10 +48,23 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({ status: 'success' });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ status: 'success', data: { tour } });
+  } catch (err) {
+    res.status(404).json({ status: 'fail', message: err.message });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(200).json({ status: 'success' });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'success', data: null });
+  } catch (err) {
+    res.status(404).json({ status: 'fail', message: err.message });
+  }
 };
--- models/tourModel.js
+++ models/tourModel.js
@@ -3,11 +3,55 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name!'],
+    required: [true, 'Name required'],
     unique: true,
+    trim: true,
   },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'A tour must have a price!'] },
+  duration: {
+    type: Number,
+    required: [true, 'Duration required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Group size required'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Dificulty required'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price required'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Summary required'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Image cover required'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
